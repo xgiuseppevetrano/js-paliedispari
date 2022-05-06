a2db5756300864fc7448155a55ef5592e4a90356
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-06 18:24:55 +0200

    new version of exercise

--- palindroma/main.js
+++ palindroma/main.js
@@ -4,15 +4,23 @@
 // 1. Chiedo all'utente di inserire una parola
 const askWord = prompt("Inserisci una parola");
 
-// 2. Creo una funzione per capire se la parola è palindroma 
-
-function checkPalindrome(word) {
-    for (let i = 0; i < word.length; i++) {
-        if (word[i] === word[word.length - 1 - i]) {
-            return "La parola è palindroma";
-        }
+// 2. Creo una funzione per invertire la mia stringa 
+function stringReverse(word) {
+    let stringReversed = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        stringReversed += word[i];
     }
-    return "La parola non è palindroma";
+    return stringReversed
 }
 
-console.log(checkPalindrome(askWord));
+// 3. Controllo se la parola inserita è uguale o no alla parola invertita
+const askWordReversed = stringReverse(askWord);
+// 3a. SE le due parole sono uguali
+//     ALLORA la parola è palindroma
+if (askWord === askWordReversed) {
+    console.log("It is a palindrome word");
+    // 3a. SE le due parole non sono uguali
+   //      ALLORA la parola non è palindroma
+} else {
+    console.log("It is not a palindrome word");
+}
