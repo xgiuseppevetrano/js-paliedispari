d5144ab73872babf9586d3dddf2676178eee7586
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-09 10:44:10 +0200

    refactoring code

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,9 +1,9 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-// 1. Chiedo all'utente di inserire una parola
-const askWord = prompt("Inserisci una parola");
-
+/*---------------
+    FUNCTION
+---------------*/
 // 2. Creo una funzione per invertire la mia stringa 
 function stringReverse(word) {
     let stringReversed = "";
@@ -13,14 +13,20 @@ function stringReverse(word) {
     return stringReversed
 }
 
+/*-----------
+    MAIN
+-----------*/
+// 1. Chiedo all'utente di inserire una parola
+const askWord = prompt("Inserisci una parola");
+    
 // 3. Controllo se la parola inserita è uguale o no alla parola invertita
 const askWordReversed = stringReverse(askWord);
 // 3a. SE le due parole sono uguali
 //     ALLORA la parola è palindroma
 if (askWord === askWordReversed) {
-    console.log("It is a palindrome word");
+    alert("It is a palindrome word");
     // 3a. SE le due parole non sono uguali
    //      ALLORA la parola non è palindroma
 } else {
-    console.log("It is not a palindrome word");
+    alert("It is not a palindrome word");
 }
